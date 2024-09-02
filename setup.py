3c6e429ff02c4a467803042ed40e0b3841be3ec1
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-09-01 22:39:22 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='conv-ae-3d',
+    version='0.1.0',
+    author='Murray Cutforth',
+    author_email='mcc4@stanford.edu',
+    description='A simple 3D convolutional autoencoder repository',
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
